6e9600291e1ac11709627035eb9f45b62dfd5d28
Kirsty Devlin <kirsty.leanne.devlin@gmail.com>
2018-07-16 22:15:44 +0100

    Adding new changes for API

--- index.js
+++ index.js
@@ -17,14 +17,8 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 app.use(function(req, res, next){
-  const whitelist = ['localhost:8080'];
-  const origin = req.headers.origin;
- // whitelist.forEach(function(val, key){
-    //if (origin.indexOf(val) > -1)
-     //res.setHeader('Access-Control-Allow-Origin', origin);
-     // res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
- /// })
-
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
@@ -36,6 +30,6 @@ app.use(router);
 
 expressListRoutes({}, 'Endpoints:', router );
 
-app.listen(3000, () => {
-  console.log('Metal Travel Guide API is running on http://localhost:3000');
+app.listen(3001, () => {
+  console.log('Metal Travel Guide API is running on http://localhost:3001');
 });
