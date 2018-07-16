ad9ccbcbc3d69700b491b3778ce919063c4b0051
Helena Nicholson <nicholson.helena@gmail.com>
2018-07-16 21:15:43 +0100

    fixing database

--- index.js
+++ index.js
@@ -19,13 +19,11 @@ app.use(methodOverride());
 app.use(function(req, res, next){
   const whitelist = ['localhost:8080'];
   const origin = req.headers.origin;
-
-  whitelist.forEach(function(val, key){
-    if (origin.indexOf(val) > -1){
-      res.setHeader('Access-Control-Allow-Origin', origin);
-      res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    }
-  })
+ // whitelist.forEach(function(val, key){
+    //if (origin.indexOf(val) > -1)
+     //res.setHeader('Access-Control-Allow-Origin', origin);
+     // res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+ /// })
 
   next();
 });
